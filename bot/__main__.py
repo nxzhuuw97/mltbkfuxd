66600cb704ba2b38a545a3d28f526d47dba10f94
nxzhuuw97 <109526391+nxzhuuw97@users.noreply.github.com>
2022-07-18 21:58:51 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -71,8 +71,8 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive or to telegram!
-Type /{BotCommands.HelpCommand} to get a list of available commands
+Ehh, Lihat Ada Om Pedo. Om Omm..., Kalo Mau Tau Cara
+Nyuruh Aku, Ketik /{BotCommands.HelpCommand} Ya Omm, seeu
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
